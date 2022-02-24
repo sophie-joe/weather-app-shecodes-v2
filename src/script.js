b85a4a4c847a01cc553d182fc6b29512c20d5a73
sophie-joe <79120183+sophie-joe@users.noreply.github.com>
2022-02-24 21:45:09 +1300

    Add in temperature converts for celsius and fahrenheit
    Convert temps and update active selection and toggling.

--- src/script.js
+++ src/script.js
@@ -49,15 +49,18 @@ function handlePosition(position) {
 
 function showMyWeather(response) {
   let cel = Math.round(response.data.main.temp);
+  celsiusTemp = Math.round(response.data.main.temp);
   let myCity = response.data.name;
   let myWind = Math.round(response.data.wind.speed);
   let myHumidity = Math.round(response.data.main.humidity);
   let myDesc = response.data.weather[0].description;
   let myLow = Math.round(response.data.main.temp_min);
+  celsiusLow = Math.round(response.data.main.temp_min);
   let myHigh = Math.round(response.data.main.temp_max);
+  celsiusHigh = Math.round(response.data.main.temp_max);
   let myCountry = response.data.sys.country;
   let myIcon = response.data.weather[0].icon;
-
+  setCelBack();
   currentTemp.innerHTML = `${cel}°C`;
   currentCity.innerHTML = `${myCity}`;
   currentWind.innerHTML = `${myWind}m/s`;
@@ -69,7 +72,7 @@ function showMyWeather(response) {
   currentDate.innerHTML = getDate(response.data.dt * 1000);
   currentIcon.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${myIcon}@2x.png`
+    `https://openweathermap.org/img/wn/${myIcon}@2x.png`
   );
   currentIcon.setAttribute("alt", `${myDesc}`);
 }
@@ -92,6 +95,13 @@ updateCurrent.addEventListener("click", callNavigationGeo);
 
 function callNavigationGeo() {
   navigator.geolocation.getCurrentPosition(handlePosition);
+}
+
+//set back celsius if fahrenheit is converted but new search started
+function setCelBack() {
+  document.getElementsByName("options")[0].checked = true;
+  fahBtn.classList.remove("active");
+  celBtn.classList.add("active");
 }
 
 //update date/time
@@ -120,10 +130,40 @@ function getDate(timestamp) {
 }
 
 //update cel to fah
-function updateCel() {
-  let input = document.querySelector("#current-temp");
-  input.innerHTML = `19`;
+function updateFah() {
+  let fahrenheitTemp = Math.round((celsiusTemp * 9) / 5 + 32);
+  let fahLow = Math.round((celsiusLow * 9) / 5 + 32);
+  let fahHigh = Math.round((celsiusHigh * 9) / 5 + 32);
+  let changeTemp = document.querySelector("#current-temp");
+  let changeFahLow = document.querySelector("#current-low-temp");
+  let changeFahHigh = document.querySelector("#current-high-temp");
+  changeTemp.innerHTML = `${fahrenheitTemp}°F`;
+  changeFahLow.innerHTML = `${fahLow}°F`;
+  changeFahHigh.innerHTML = `${fahHigh}°F`;
+  fahBtn.classList.add("active");
+  celBtn.classList.remove("active");
 }
 
-let changeCel = document.querySelector("#option1");
-changeCel.addEventListener("change", updateCel);
+function updateCel() {
+  let changeTemp = document.querySelector("#current-temp");
+  let changeCelLow = document.querySelector("#current-low-temp");
+  let changeCelHigh = document.querySelector("#current-high-temp");
+  changeTemp.innerHTML = `${celsiusTemp}°C`;
+  changeCelHigh.innerHTML = `${celsiusHigh}°C`;
+  changeCelLow.innerHTML = `${celsiusLow}°C`;
+  fahBtn.classList.remove("active");
+  celBtn.classList.add("active");
+}
+
+let changeFah = document.querySelector(`#fah`);
+changeFah.addEventListener("click", updateFah);
+
+let fahBtn = document.querySelector(`#fahBtn`);
+let celBtn = document.querySelector(`#celBtn`);
+
+let changeCel = document.querySelector(`#cel`);
+changeCel.addEventListener("click", updateCel);
+
+let celsiusTemp = null;
+let celsiusLow = null;
+let celsiusHigh = null;
